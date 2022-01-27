e76aa0d30f029f48b371d81cb609b56a1d208424
Winter <36679448+GentleWinter@users.noreply.github.com>
2022-01-26 22:30:33 -0300

    senha e sistema de autenticação (não passando pela matricula?)

---
+++ testeFuncionario.js
@@ -0,0 +1,14 @@
+//import { Cliente } from "./Cliente.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
+
+const diretor = new Diretor("Gerson", 12235, 12345678980, 5000);
+const gerente = new Gerente("Fabio", 55789, 12345678990, 3000);
+
+diretor.cadastrarSenha("123456");
+
+const estaLogado = SistemaAutenticacao.login(diretor, diretor._senha);
+
+console.log(estaLogado);
+console.log(diretor._senha);
