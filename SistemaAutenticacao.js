a0ff710e565f2e6ae8a5b0d74695c6c09d6f2afd
Winter <36679448+GentleWinter@users.noreply.github.com>
2022-01-27 21:02:37 -0300

    DuckType

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -1,11 +1,13 @@
 export class SistemaAutenticacao{
     static login(autenticavel, senha){
+        //Duck Type
         if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
             return autenticavel.autenticar(senha);
         }
         return false;
     }
 
+    //Duck Type
     static ehAutenticavel(autenticavel){
         return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function;
     }
